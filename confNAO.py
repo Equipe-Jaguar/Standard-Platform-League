1cf8dae6cbc567a482f2d76b6584e06770133148
Helton Sereno <hrsereno@gmail.com>
2020-06-03 22:28:56 -0300

    Versão Game Controler
    As definições de Team_number vem do programa onde o número 50 corresponde a nossa Equipe Jaguar
    
    O player númber deve variar para cada jogador sendo que o 1º player (Jodaor 1) possui o número 0
    
    O ip_GM é o broadcast do game controler, devemos manter os 3 primiros números de IP da mesma rede onde o NAO está no caso de uma rede doméstica, mantendo somente o 255 no finals

--- confNAO.py
+++ confNAO.py
@@ -3,10 +3,11 @@
 ip_addr = "127.0.0.1"
 port_num = 9559
 
-'''
-Definições:
-- Olhos direitos para o GameControler
-- Olhos esquerdo para achou a bola
-'''
+#'''
+#Definicoes:
+#- Olhos esquerdo para o GameControler
+#- Olhos direito para achou a bola
+#'''
 team_number = 50
-player_number = 1
+player_number = 0
+ip_gm = '192.168.0.255' #ip do broadcast

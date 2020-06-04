d3116ab21f90a9b90d305ea1a38490e7e8aab98f
Helton Sereno <hrsereno@gmail.com>
2020-06-03 22:25:13 -0300

    Primeira versão
    Primeira versão do arquivo de comunicação com o Game Controler.
    Nesta versão, é escrito na memória do robô o estado da partida, tempo restante e se o robô está penalizado.
    Finalizando o processo quando o robô recebe a indormação de STATE_FINISHED correspondente ao fim da partida.

--- communication.py
+++ communication.py
@@ -7,6 +7,11 @@ from gamestate import GameState, RobotInfo, ReturnData, GAME_CONTROLLER_RESPONSE
 #import logging
 #import unboard
 import confNAO
+from naoqi import ALProxy, ALModule
+
+leds = ALProxy('ALLeds', confNAO.ip_addr, 9559)
+memoria = ALProxy('ALMemory', confNAO.ip_addr, 9559)
+#leds.off('AllLeds')
 
 #session para poder se inscrever
 session = qi.Session()
@@ -19,12 +24,23 @@ GAME_CONTROLLER_ANSWER_PORT = 3939
 def main():
     udpSocket = socket(AF_INET, SOCK_DGRAM)
     try:
-        #tenta bindar no endereço de broadcast da rede, muda de acordo com as configurações dela. Se o programa não for matado corretamente (ex. dar ctrl+c em vez de ctrl+\), não vai dar pra bindar nessa porta, pois ela já vai estar ocupada
-        udpSocket.bind(('169.254.255.255', GAME_CONTROLLER_LISTEN_PORT))
+        #tenta bindar no endereco de broadcast da rede, muda de acordo com as
+        #configuracoes dela. Se o programa nao for matado corretamente (
+        #ex. dar ctrl+c em vez de ctrl+\), nao vai dar pra bindar nessa porta,
+        #pois ela ja vai estar ocupada
+        #leds.off('LeftFaceLeds')
+        leds.off('LeftFaceLeds')
+    	leds.on('LeftFaceLedsGreen')
+        #leds.on('LefFootLedsGreen')
+        udpSocket.bind((confNAO.ip_gm, GAME_CONTROLLER_LISTEN_PORT))
+        print("Conectou!!!")
     except Exception as identifier:
-        logging.exception("Bind error")
-        if (identifier.errno == 98):  #addr already in use
-            return
+        print("Bind error")
+        #leds.off('LeftFaceLeds')
+        leds.on('LeftFaceLedsRed')
+        #leds.on('LeftFootLedsRed')
+        #if (identifier.erro == 98):  #addr already in use
+        #    return
     try:
         while (1):
             #tenta catar pacotes com o tamanho setado no game controller
@@ -46,18 +62,26 @@ def main():
             #     logging.info("key: {} in struct recieved is {}".format(
             #         structKey, structValue))
 
-            #loggando alguns dados para saber se está recebendo o pacote corretamente
-            logging.debug("gamephase : {}".format(parsed_state.gamePhase))
-            logging.debug("game_state : {}".format(parsed_state.game_state))
-            logging.debug("first_half : {}".format(parsed_state.first_half))
-            logging.debug("secsRemaining : {}".format(parsed_state.secsRemaining))
-            logging.debug("penalty : {}".format(parsed_state.teams[teamNumber].players[confNAO.player_numbe].penalty))
+            #loggando alguns dados para saber se esta recebendo o pacote corretamente
+            #print("gamephase : {}".format(parsed_state.gamePhase))
+            #print("game_state : {}".format(parsed_state.game_state))
+            memoria.insertData("gamestate",parsed_state.game_state)
+            #print("first_half : {}".format(parsed_state.first_half))
+            #print("secsRemaining : {}".format(parsed_state.secsRemaining))
+            memoria.insertData("secsRemaining",parsed_state.secsRemaining)
+            #print("penalty : {}".format(parsed_state.teams[teamNumber].players[confNAO.player_number].penalty))
+            memoria.insertData("penalty",parsed_state.teams[teamNumber].players[confNAO.player_number].penalty)
+            #print("pickup : {}".format(parsed_state.teams[teamNumber].players[confNAO.player_number].pick_up))
 
+            if parsed_state.game_state == "STATE_FINISHED":
+                leds.off('LeftFaceLeds')
+                leds.on('LeftFaceLedsRed')
+                break
 
             #update unboard
-            unboard.gameState = parsed_state.game_state
+            #print(parsed_state.game_state)
             # print("1", unboard.gameState)
-            unboard.penalty = parsed_state.teams[teamNumber].players[confNAO.player_numbe].penalty
+            #print( parsed_state.teams[teamNumber].players[confNAO.player_number].penalty)
 
             #dados de retorn para o GC
             data = Container(
@@ -71,11 +95,14 @@ def main():
                 destination = peer[0], GAME_CONTROLLER_ANSWER_PORT
                 udpSocket.sendto(ReturnData.build(data), destination)
             except Exception as e:
-                logger.exception("Network Error: %s" % str(e))
+                print("Network Error: %s" % str(e))
     finally:
-        #fecha a conexão se o codigo sair do while para poder ser reaberta dps
+        #fecha a conexao se o codigo sair do while para poder ser reaberta dps
         try:
             udpSocket.shutdown(SHUT_RDWR)
         except Exception as ex:
-            logging.exception("could not shutdown")
+            print("could not shutdown")
         udpSocket.close()
+
+if __name__ == '__main__':
+    main()
